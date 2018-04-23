a4d1d9b0ae2868f92a15ee2055e5f46a767515d4
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+using DevExpress.XtraScheduler;
+
+namespace SchedulerGridSplitterWpf {
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window {
+        public MainWindow() {
+            InitializeComponent();
+
+            GenerateResources();
+        }
+
+        private void GenerateResources() {
+            schedulerControl1.Storage.ResourceStorage.BeginUpdate();
+            schedulerControl1.Storage.ResourceStorage.Add(new Resource(0, "First Resource"));
+            schedulerControl1.Storage.ResourceStorage.Add(new Resource(1, "Second Resource"));
+            schedulerControl1.Storage.ResourceStorage.Add(new Resource(2, "Third Resource"));
+            schedulerControl1.Storage.ResourceStorage.EndUpdate();
+        }
+    }
+}
